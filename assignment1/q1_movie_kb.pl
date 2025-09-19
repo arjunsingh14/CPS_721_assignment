661c70643b690a7a8ec1f1bad7db78ac09d449f5
Arjun Bhandal <arjunsingh140601@gmail.com>
2025-09-19 12:26:38 -0400

    Initial commit

---
+++ assignment1/q1_movie_kb.pl
@@ -0,0 +1,84 @@
+% Enter the names of ID of your group members below.
+% For STUDENT ID, use your 9-digit ONECARD number.
+% If you have 2 group members, leave the last entry blank.
+%
+%%%%%
+%%%%% NAME: Golsa Momeni
+%%%%% STUDENT ID: 501202209
+%%%%%
+%%%%% NAME: Bardia Shirsalimian
+%%%%% STUDENT ID:
+%%%%%
+%%%%% NAME: Arjun Bhandal
+%%%%% STUDENT ID:
+%
+% Add the required KB statements in the section below.
+% Do NOT delete, edit, or add SECTION headers.
+% If you put statements in the wrong section, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY 
+% the already included comment lines below
+
+
+%%%%% SECTION: q1_kb
+%%%%% You should put the atomic statements for your KB below
+
+    
+releaseInfo(jaws, 1975, 164).
+releaseInfo(glass_onion, 2022, 139).
+releaseInfo(oppenheimer, 2023, 180).
+releaseInfo(midsommar, 2019, 171).
+releaseInfo(the_godfather, 1972, 175).
+releaseInfo(the_godfather_part_2, 1974, 202).
+releaseInfo(epic_crime, 1974, 185).
+releaseInfo(take_this_waltz, 2011, 116).
+releaseInfo(serpico, 1973, 130).
+releaseInfo(black_bag, 2015, 93).
+releaseInfo(tar, 2022, 158).
+releaseInfo(happy_hour, 1950, 117).
+releaseInfo(maybe_one_day, 2024, 140).
+releaseInfo(paris_nights, 2023, 141).
+releaseInfo(best_days, 1951, 139).
+releaseInfo(good_times, 2021, 138). 
+
+directedBy(jaws, steven_spielberg).
+directedBy(glass_onion, rian_johnson).
+directedBy(oppenheimer, christopher_nolan).
+directedBy(midsommar, ari_aster).
+directedBy(the_godfather, francis_ford_coppola).
+directedBy(the_godfather_part_2, francis_ford_coppola).
+directedBy(epic_crime, martin_scorsese).
+directedBy(take_this_waltz, sarah_polley).
+directedBy(serpico, sidney_lumet).
+directedBy(black_bag, steven_soderbergh).
+directedBy(tar, todd_field).
+directedBy(happy_hour, emily_stone).
+directedBy(maybe_one_day, emily_stone).
+directedBy(paris_nights, kate_stevens).
+directedBy(best_days, sasha_brown).
+directedBy(good_times, sasha_brown).
+    
+actedIn(roy_scheider, jaws, martin_brody).
+actedIn(janelle_monae, glass_onion, andi_brand).
+actedIn(florence_pugh, oppenheimer, jean_tatlock).
+actedIn(florence_pugh, midsommar, dani_ardor).
+actedIn(al_pacino, the_godfather, michael_corleone).
+actedIn(james_caan, the_godfather, sonny_corleone).
+actedIn(diane_keaton, the_godfather, kay_adams).
+actedIn(al_pacino, the_godfather_part_2, michael_corleone).
+actedIn(robert_de_niro, the_godfather_part_2, vito_corleone).
+actedIn(al_pacino, epic_crime, lead_mobster).
+actedIn(seth_rogen, take_this_waltz, lou_rubin).
+actedIn(al_pacino, serpico, frank_serpico).
+actedIn(cate_blanchett, black_bag, kathryn_jean).
+actedIn(cate_blanchett, tar, lydia_tar).
+actedIn(kristen_stweart, happy_hour, sasha_james).
+actedIn(robert_pattinson, maybe_one_day, adam_jones).
+actedIn(florence_pugh, paris_nights, lily_baker).   
+actedIn(emma_watson, best_days, kate_johnson).
+actedIn(tom_hanks, good_times, noah_davis).
+    
+
+
+%%%%% END
+% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
