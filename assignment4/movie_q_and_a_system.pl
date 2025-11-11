790cce64f787dc401a460997f39095a59669c755
Arjun Bhandal <arjunsingh140601@gmail.com>
2025-11-11 09:10:09 -0500

    P3 finished

--- assignment4/movie_q_and_a_system.pl
+++ assignment4/movie_q_and_a_system.pl
@@ -150,6 +150,125 @@ strings_to_atoms([String | RestStrings], [Atom | RestAtoms]) :-
 % ie. " hello    world how are  you   " becomes [hello, world, how, are, you]
 atom_list_from_string(WordsString, AtomList) :-
    split_string(WordsString, " ", " ", WordList), strings_to_atoms(WordList, AtomList).
+
+article(a).
+article(an).
+article(the).
+article(any).
+
+common_noun(movie, What) :- movie(What).
+common_noun(film, What) :- movie(What).
+common_noun(actor, What) :- actor(What).
+common_noun(director, What) :- director(What).
+common_noun(character, What) :- character(What).
+common_noun(length, What) :- movieLength(What).
+common_noun(running_time, What) :- movieLength(What).
+common_noun(genre, What) :- genre(What).
+common_noun(release_year, What) :- releaseYear(What).
+
+adjective(three_hour, What) :-
+    movie(What),
+    releaseInfo(What, _, Length),
+    Length >= 180.
+
+adjective(short, What) :-
+    movie(What),
+    releaseInfo(What, _, Length),
+    Length < 60.
+
+adjective(new, What) :-
+    movie(What),
+    currentYear(Y),
+    releaseInfo(What, Y, _).
+
+adjective(new, What) :-
+    actor(What),
+    newActor(What).
+
+adjective(new, What) :-
+    director(What),
+    newDirector(What).
+
+adjective(Adj, What) :-
+    genre(Adj),
+    movie(What),
+    movieGenre(What, Adj).
+
+adjective(Adj, What) :-
+    genre(Adj),
+    actor(What),
+    genreActor(What, Adj).
+
+adjective(Adj, What) :-
+    genre(Adj),
+    director(What),
+    genreDirector(What, Adj).
+
+adjective(Adj, What) :-
+    director(Adj),
+    movie(What),
+    directedBy(What, Adj).
+
+adjective(Adj, What) :-
+    actor(Adj),
+    movie(What),
+    actedIn(Adj, What, _).
+
+proper_noun(Name) :- movie(Name).
+proper_noun(Name) :- actor(Name).
+proper_noun(Name) :- director(Name).
+proper_noun(Name) :- character(Name).
+proper_noun(Num) :- number(Num).
+
+preposition(by, What, Ref) :-
+    movie(What),
+    director(Ref),
+    directedBy(What, Ref).
+
+preposition(with, What, Ref) :-
+    movie(What),
+    actor(Ref),
+    actedIn(Ref, What, _).
+
+preposition(with, What, Ref) :-
+    movie(What),
+    character(Ref),
+    actedIn(_, What, Ref).
+
+preposition(in, What, Ref) :-
+    actor(What),
+    movie(Ref),
+    actedIn(What, Ref, _).
+
+preposition(in, What, Ref) :-
+    character(What),
+    movie(Ref),
+    actedIn(_, Ref, What).
+
+preposition(from, What, Ref) :-
+    movie(What),
+    releaseYear(Ref),
+    releaseInfo(What, Ref, _).
+
+preposition(released_in, What, Ref) :-
+    movie(What),
+    releaseYear(Ref),
+    releaseInfo(What, Ref, _).
+
+preposition(played_by, What, Ref) :-
+    character(What),
+    actor(Ref),
+    actedIn(Ref, _, What).
+
+preposition(of, What, Ref) :-
+    movieLength(What),
+    movie(Ref),
+    releaseInfo(Ref, _, What).
+
+preposition(of, What, Ref) :-
+    releaseYear(What),
+    movie(Ref),
+    releaseInfo(Ref, What, _).
    
 
 
